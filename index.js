553bb7a2e65d6a1fed12e777cb9623c3e52b7fb1
Adriano Dutra <adrianoo.r.d@gmail.com>
2021-01-24 00:06:48 -0300

    version v1.0.3

--- index.js
+++ index.js
@@ -74,11 +74,10 @@ async function validacaoDeEntrada(data){
     const prioridades = ['normal', 'urgente'];
     const tiposAtividades = ['manutencao','documentacao','desenvolvimento'];
     if(data.nome.length < 3) return 'Digite o seu nome';
-    
+
     if(!tiposAtividades.includes(data.atividade)) return 'Atividade não existe';
     if(!prioridades.includes(data.prioridade)) return 'Prioridade não existe';
     if(!moment(data.dtrealizacao, "YYYY-MM-DD", true).isValid()) return 'Data inválida';
-
     return true;
 }
 
